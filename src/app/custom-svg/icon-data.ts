f768270da52b8272aadbdfbc594c00f3ab07a92e
mirzahab85 <mirza.habul@telemach.ba>
2022-10-18 22:23:25 +0200

    Updating header using svg icons

--- src/app/custom-svg/icon-data.ts
+++ src/app/custom-svg/icon-data.ts
@@ -1,16 +1,45 @@
 export const Icondata = [
 
   {
-      name: 'arrow',
-      url: `./assets/icon.svg`
+      name: 'notification',
+      url: `./assets/icons/notification.svg`,
   },
   {
-      name: 'test',
-      url: `./assets/icon1.svg`
+      name: 'fork',
+      url: `./assets/icons/fork.svg`,
   },
   {
-      name: 'nova',
-      url: `./assets/icon2.svg`
-  }
+      name: 'star',
+      url: `./assets/icons/star.svg`,
+  },
+  {
+    name: 'code',
+    url: `/assets/icons/code.svg`,
+  },
+  {
+    name: 'adjust',
+    url: `/assets/icons/adjust.svg`,
+  },
+  {
+    name: 'pull_request',
+    url: `/assets/icons/pull_request.svg`,
+  },
+  {
+    name: 'actions',
+    url: `/assets/icons/actions.svg`,
+  },
+  {
+    name: 'projects',
+    url: `/assets/icons/projects.svg`,
+  },
+  {
+    name: 'security',
+    url: `/assets/icons/security.svg`,
+  },
+  {
+    name: 'insights',
+    url: `/assets/icons/insights.svg`,
+  },
+
 
 ]
