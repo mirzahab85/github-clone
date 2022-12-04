4e496f692ce260def6ff7cdb2e808a3e74cde8a9
mirzahab85 <mirza.habul@telemach.ba>
2022-12-04 10:36:00 +0100

    Routing pages and navigation

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeaderRoutingModule } from './header-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderRoutingModule
   ]
 })
 export class HeaderModule { }
